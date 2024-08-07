b772ffd6f2e91d1e6cd345656b41b9828271bd4e
Esraaismail309 <esraaismail309@gmail.com>
2024-08-07 13:38:32 +0300

    test new branch

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import styles from "./Cart.module.css";
 import { Helmet } from "react-helmet";
 
-
 export default function Cart(props) {
   console.log(props);
-  
-  return <div>
 
-<Helmet>
-       
-       <title>cart</title>
-     </Helmet>
-  </div>;
+  return (
+    <div>
+      <Helmet>
+        <title>cart</title>
+      </Helmet>
+    </div>
+  );
 }
